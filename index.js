0a07892ac80d0dc207c2a1d1a604ec0422e17f4c
zuleika <njokizuleika@gmail.com>
2023-02-27 13:31:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Sam',
+    streetAddress : '12 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let newEmployee={...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee,key){
+    let newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+  }
